bdd652095f53953ed28c649bf468c39ed406d244
Markus Grech <me@mgrech.dev>
2019-05-04 17:03:12 +0200

    Setup TypeScript

---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+
+export
+function activate(context: vscode.ExtensionContext)
+{
+	vscode.window.showInformationMessage("Hello, Katana!");
+}
+
+export
+function deactivate()
+{}
